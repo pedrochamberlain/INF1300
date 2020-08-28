a711591017d04a84871ed67117ef7521b94c65bc
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-08-28 16:30:32 -0300

    envelop red container with MaterialApp and Scaffold widgets

--- udacity/hello_rectangle/lib/main.dart
+++ udacity/hello_rectangle/lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Container(
-      color: Colors.redAccent,
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: Container(
+          color: Colors.redAccent,
+        ),
+      ),
     ),
   );
 }
