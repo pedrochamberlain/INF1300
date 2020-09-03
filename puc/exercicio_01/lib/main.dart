b67475ffa49b5fa8eb6a829db4e3d0e0cce07466
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-03 10:09:15 -0300

    import course exercise 1

---
+++ puc/exercicio_01/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Exercício 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Exercício 1 - Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key key,
+    this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Card(
+        child: Column(
+          children: <Widget>[
+            Image.asset('assets/burger.jpg'),
+            Text('Hamburguer')
+          ],
+        ),
+      ),
+    );
+  }
+}
