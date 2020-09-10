e0607fd129184d820eed21e9bb83acab343358fb
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-10 15:14:56 -0300

    turn stateless widgets stateful

--- udacity/task_05_stateful_widgets/lib/converter_route.dart
+++ udacity/task_05_stateful_widgets/lib/converter_route.dart
@@ -13,8 +13,7 @@ import 'package:task_05_stateful_widgets/unit.dart';
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
-// TODO: Make ConverterRoute a StatefulWidget
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   /// Color for this [Category].
   final Color color;
 
@@ -28,27 +27,28 @@ class ConverterRoute extends StatelessWidget {
   })  : assert(color != null),
         assert(units != null);
 
-  // TODO: Create State object for the ConverterRoute
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
 
+class _ConverterRouteState extends State<ConverterRoute> {
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
-    // TODO: Once the build() function is inside the State object,
-    // you'll have to reference this using `widget.units`
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
             Text(
               unit.name,
-              style: Theme.of(context).textTheme.headline,
+              style: Theme.of(context).textTheme.headline5,
             ),
             Text(
               'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subhead,
+              style: Theme.of(context).textTheme.subtitle1,
             ),
           ],
         ),
