4ffbb70e02db298638e8bcb8cf599a595dd06ee1
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-26 17:14:42 -0300

    create app interface

--- udemy/gas_price/lib/home.dart
+++ udemy/gas_price/lib/home.dart
@@ -6,12 +6,83 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _result = "";
+  TextEditingController _controllerAlcohol = TextEditingController();
+  TextEditingController _controllerGasoline = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Álcool ou Gasolina?"),
         backgroundColor: Colors.lightBlue,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset('images/logo.png')),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  'Saiba qual é a melhor opção para seu carro:',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Preço do Álcool'),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerAlcohol,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: 'Preço da Gasolina'),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerGasoline,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                child: RaisedButton(
+                  onPressed: () => {},
+                  color: Colors.lightBlue,
+                  textColor: Colors.white,
+                  child: Text(
+                    'Calcular',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      _result,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- udemy/gas_price/lib/main.dart
+++ udemy/gas_price/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:gas_price/home.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     ),
   );
