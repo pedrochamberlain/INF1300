973309c1a98ecc7ce15a591e4d277b55ab65ee5c
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-26 17:38:21 -0300

    add app functionality

--- udemy/gas_price/lib/home.dart
+++ udemy/gas_price/lib/home.dart
@@ -10,12 +10,36 @@ class _HomeState extends State<Home> {
   TextEditingController _controllerAlcohol = TextEditingController();
   TextEditingController _controllerGasoline = TextEditingController();
 
+  void _calculate() {
+    double alcoholPrice = double.tryParse(_controllerAlcohol.text);
+    double gasolinePrice = double.tryParse(_controllerGasoline.text);
+
+    if (alcoholPrice <= 0.0 ||
+        alcoholPrice == null ||
+        gasolinePrice <= 0.0 ||
+        gasolinePrice == null) {
+      setState(() {
+        _result = "Valor inválido";
+      });
+    } else {
+      if ((alcoholPrice / gasolinePrice) >= 0.7) {
+        setState(() {
+          _result = 'Melhor abastecer com gasolina';
+        });
+      } else {
+        setState(() {
+          _result = 'Melhor abastecer com gasolina';
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Álcool ou Gasolina?"),
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.blueAccent,
       ),
       body: Container(
         child: SingleChildScrollView(
@@ -30,6 +54,7 @@ class _HomeState extends State<Home> {
                 padding: EdgeInsets.only(bottom: 10),
                 child: Text(
                   'Saiba qual é a melhor opção para seu carro:',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
@@ -55,8 +80,8 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: EdgeInsets.only(top: 20, bottom: 20),
                 child: RaisedButton(
-                  onPressed: () => {},
-                  color: Colors.lightBlue,
+                  onPressed: this._calculate,
+                  color: Colors.blueAccent,
                   textColor: Colors.white,
                   child: Text(
                     'Calcular',
@@ -66,16 +91,15 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ),
-              Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  child: SingleChildScrollView(
-                    child: Text(
-                      _result,
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: SingleChildScrollView(
+                  child: Text(
+                    this._result,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
