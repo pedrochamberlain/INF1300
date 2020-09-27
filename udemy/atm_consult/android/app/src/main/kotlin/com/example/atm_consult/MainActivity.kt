440fb6fe85b4e892acc6ad810b2bf24a75cc64c4
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-27 10:47:39 -0300

    create atm consult project

---
+++ udemy/atm_consult/android/app/src/main/kotlin/com/example/atm_consult/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atm_consult
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udemy/atm_consult/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:atm_consult/home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
