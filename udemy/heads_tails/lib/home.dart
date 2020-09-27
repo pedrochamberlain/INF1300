e1142d8c9a9eaf6775dbd229a8f0cb3b5e0131cb
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-27 13:06:37 -0300

    create app home interface

---
+++ udemy/heads_tails/lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void _accessResult() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset('images/logo.png'),
+            GestureDetector(
+              onTap: () => this._accessResult,
+              child: Image.asset('images/botao_jogar.png'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
