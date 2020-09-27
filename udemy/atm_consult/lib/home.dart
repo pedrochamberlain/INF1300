07369faffa6f5e705651906b9d8ff881beb96eba
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-09-27 11:00:09 -0300

    create interface

--- udemy/atm_consult/lib/home.dart
+++ udemy/atm_consult/lib/home.dart
@@ -6,6 +6,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void _accessEmpresa() {}
+
+  void _accessServico() {}
+
+  void _accessCliente() {}
+
+  void _accessContato() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,7 +21,48 @@ class _HomeState extends State<Home> {
         title: Text('ATM Consultoria'),
         backgroundColor: Colors.green,
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('images/logo.png'),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: this._accessEmpresa,
+                    child: Image.asset('images/menu_empresa.png'),
+                  ),
+                  GestureDetector(
+                    onTap: this._accessServico,
+                    child: Image.asset('images/menu_servico.png'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: this._accessCliente,
+                    child: Image.asset('images/menu_cliente.png'),
+                  ),
+                  GestureDetector(
+                    onTap: this._accessContato,
+                    child: Image.asset('images/menu_contato.png'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
