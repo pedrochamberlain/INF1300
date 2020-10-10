3b5d8f6bb9aea1d85ad00d5fbc64bf9a92dec2dd
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-10-10 18:32:40 -0300

    add video query

--- src/lib/widgets/mediascroller.dart
+++ src/lib/widgets/mediascroller.dart
@@ -23,12 +23,61 @@ class MediaScroller extends StatelessWidget {
             case ConnectionState.none:
             case ConnectionState.waiting:
               return Center(
-                child: CircularProgressIndicator(),
+                child: Padding(
+                  padding: const EdgeInsets.all(57.0),
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.black,
+                  ),
+                ),
               );
 
             case ConnectionState.active:
             case ConnectionState.done:
               if (snapshot.hasData) {
+                return Container(
+                  child: Expanded(
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        Video video = snapshot.data[index];
+
+                        return GestureDetector(
+                          onTap: () => {
+                            FlutterYoutube.playYoutubeVideoById(
+                              apiKey: YOUTUBE_API_KEY,
+                              videoId: video.id,
+                              autoPlay: true,
+                              fullScreen: true,
+                            )
+                          },
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(3.0, 12.0, 3.0, 16.0),
+                            child: Card(
+                              color: Colors.yellow,
+                              child: Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(video.img),
+                                  ),
+                                ),
+                                child: Text(video.title),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) => Divider(
+                        height: 0,
+                        color: Colors.transparent,
+                      ),
+                      itemCount: snapshot.data.length,
+                    ),
+                  ),
+                );
               } else {
                 return Container(
                   height: 150,
