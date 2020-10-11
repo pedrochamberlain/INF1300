8dd2761f266b108c21470e71b4b6f71e7c4a8e5b
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-10-11 20:02:38 -0300

    add player controls

--- udemy/player/lib/home.dart
+++ udemy/player/lib/home.dart
@@ -1,5 +1,5 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,18 +7,72 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool firstPlay = true;
   AudioPlayer player = AudioPlayer();
 
   _play() async {
     String url =
         "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
-    await player.play(url);
+
+    if (firstPlay) {
+      await player.play(url);
+      firstPlay = false;
+    } else {
+      player.resume();
+    }
+  }
+
+  _pause() async {
+    await player.pause();
+  }
+
+  _stop() async {
+    await player.stop();
   }
 
   @override
   Widget build(BuildContext context) {
-    _play();
-
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // TODO: Slider
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () => _play(),
+                  child: Icon(
+                    Icons.play_arrow_rounded,
+                    size: 24.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () => _pause(),
+                  child: Icon(
+                    Icons.pause_rounded,
+                    size: 24.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  onTap: () => _stop(),
+                  child: Icon(
+                    Icons.stop_rounded,
+                    size: 24.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
