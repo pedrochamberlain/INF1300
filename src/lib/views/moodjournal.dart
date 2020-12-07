856d2a894da7a705b41e2ab4f3521c4550b3b868
Pedro Chamberlain <pedrochmatos@gmail.com>
2020-12-07 18:48:27 -0300

    style(mood_board): lint fix

--- src/lib/views/moodjournal.dart
+++ src/lib/views/moodjournal.dart
@@ -18,20 +18,25 @@ class _MoodJournalState extends State<MoodJournal> {
       children: [
         Expanded(
           child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: history.entries.length,
-              itemBuilder: (context, index) {
-                var entry = history.entries[index];
-                var activities = List<Text>();
-                for (var activity in entry.activities) {
-                  activities.add(Text(activity));
-                }
-                return Card(
-                    color: Colors.purple[200],
-                    child: ListTile(
-                        title: Text(entry.mood),
-                        subtitle: Column(children: activities)));
-              }),
+            scrollDirection: Axis.vertical,
+            itemCount: history.entries.length,
+            itemBuilder: (context, index) {
+              var entry = history.entries[index];
+              var activities = List<Text>();
+              for (var activity in entry.activities) {
+                activities.add(
+                  Text(activity),
+                );
+              }
+              return Card(
+                color: Colors.purple[200],
+                child: ListTile(
+                  title: Text(entry.mood),
+                  subtitle: Column(children: activities),
+                ),
+              );
+            },
+          ),
         ),
         Padding(
           padding: const EdgeInsets.only(top: 32.0, bottom: 32.0),
@@ -48,33 +53,44 @@ class _MoodJournalState extends State<MoodJournal> {
 
   newEntry(context) {
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(AppLocalizations.of(context).feeling),
-            content: TextField(
-                decoration: InputDecoration(
-                  labelText: AppLocalizations.of(context).feeling_desc,
-                ),
-                onChanged: (text) {
-                  _text = text;
-                }),
-            actions: [
-              FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context);
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context).feeling),
+          content: TextField(
+            decoration: InputDecoration(
+              labelText: AppLocalizations.of(context).feeling_desc,
+            ),
+            onChanged: (text) {
+              _text = text;
+            },
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancelar"),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+                setState(
+                  () {
+                    history.addEntry(
+                      MoodEntry(
+                        _text,
+                        List(),
+                      ),
+                    );
                   },
-                  child: Text("Cancelar")),
-              FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    setState(() {
-                      history.addEntry(MoodEntry(_text, List()));
-                    });
-                  },
-                  child: Text("Confirmar"))
-            ],
-          );
-        });
+                );
+              },
+              child: Text("Confirmar"),
+            )
+          ],
+        );
+      },
+    );
   }
 }
